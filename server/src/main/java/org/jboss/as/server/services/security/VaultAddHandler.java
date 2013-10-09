96858660e81727fdb8e1003aa774ad4b5ed800ee
Navin Surtani <nsurtani@redhat.com>
2013-10-09 15:38:58 +0800

    WFLY-1904 More work done

--- server/src/main/java/org/jboss/as/server/services/security/VaultAddHandler.java
+++ server/src/main/java/org/jboss/as/server/services/security/VaultAddHandler.java
@@ -26,6 +26,7 @@ import org.jboss.as.controller.AbstractAddStepHandler;
 import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.server.ServerMessages;
 import org.jboss.as.server.controller.resources.VaultResourceDefinition;
@@ -65,7 +66,7 @@ public class VaultAddHandler extends AbstractAddStepHandler {
     @Override
     protected void performRuntime(OperationContext context, ModelNode operation, ModelNode model, ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers) throws OperationFailedException {
         // Adding as a separate call here since we can understand better when this call has to be made.
-        addNewRuntimeStep(context, verificationHandler);
+        addNewRuntimeStep(context);
 
         ModelNode codeNode = VaultResourceDefinition.CODE.resolveModelAttribute(context, model);
         String vaultClass = codeNode.isDefined() ? codeNode.asString() : null;
@@ -85,8 +86,14 @@ public class VaultAddHandler extends AbstractAddStepHandler {
         }
     }
 
-    private void addNewRuntimeStep(OperationContext context, ServiceVerificationHandler verificationHandler) {
-        context.addStep(verificationHandler, OperationContext.Stage.RUNTIME, true);
+    private void addNewRuntimeStep(OperationContext context) {
+        context.addStep(new OperationStepHandler() {
+           @Override
+           public void execute(OperationContext context, ModelNode operation) throws OperationFailedException {
+              //To change body of implemented methods use File | Settings | File Templates.
+           }
+        }, OperationContext.Stage.RUNTIME,
+              true);
     }
 
     @Override
